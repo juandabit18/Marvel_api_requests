59d083e6667b5c2acb04ad59b060b5acf5917141
Juan David Jimenez <juanda18@gmail.com>
2021-05-15 10:41:44 -0500

    webbrowser

--- api_call.py
+++ api_call.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3.8
+#!/usr/bin/python3
 
 import hashlib, requests, webbrowser, shutil, json
 
@@ -10,18 +10,30 @@ hash = hashlib.md5(suma.encode("utf-8")).hexdigest()
 
 parameter = {"apikey": puk, "ts": ts, "hash": hash}
 
-# r = requests.get("http://gateway.marvel.com/v1/public/characters", parameter)
-#
-# packages_json = r.json()
-#
-# packages_cute = json.dumps(packages_json, indent=2)
+r = requests.get("http://gateway.marvel.com/v1/public/characters", parameter)
+
+packages_json = r.json()
+
+packages_cute = json.dumps(packages_json, indent=2)
 #print(packages_cute)
 
-data = input("Enter the name of the character: ")
+#data = input("Enter the name of the character: ")
+data = "spider-man"
 
 package_url = "http://gateway.marvel.com/v1/public/characters?nameStartsWith={}".format(data)
 print("The url to search for the character is:", package_url)
 r = requests.get(package_url, parameter)
 package_json = r.json()
-package_cute = json.dumps(package_json, indent=2)
-print("The information of the character is:\n", package_cute)
+package_str_cute = json.dumps(package_json, indent=2)
+
+package_dr = package_json["data"]["results"][0]
+package_dr_str_cute = json.dumps(package_dr, indent=2)
+
+character_name = package_dr["name"]
+character_description = package_dr["description"]
+
+print("The information of the character is:\nName: {}\nDescription: {}".format(character_name, character_description))
+
+image_url = package_dr["thumbnail"]["path"]+".jpg"
+
+webbrowser.open_new_tab(image_url)
